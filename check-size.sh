10ca0f75e77f5a1c982c2c55f0955b7f389f0c21
Ryan Baumann <ryan.baumann@gmail.com>
2016-01-15 10:03:31 -0500

    Initial changes for OCR

--- check-size.sh
+++ check-size.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
 
+CONVERT_OPTIONS="-type Grayscale -background white +matte -depth 32"
 declare -a extensions=("_jp2.zip" "_tif.zip" "_raw_jp2.zip" ".pdf" "_bw.pdf")
 
 for extension in "${extensions[@]}"; do
-  curl_output=$(curl --fail -L -I "https://archive.org/download/${1}/${1}${extension}" 2>&1)
+  url="https://archive.org/download/${1}/${1}${extension}"
+  curl_output=$(curl --fail -L -I "${url}" 2>&1)
   if [ $? -eq 0 ]; then
-    # echo "$curl_output"
-    content_length=$(echo "$curl_output" | grep 'Content-Length' | tail -1 | cut -d' ' -f2)
-    echo -e "$1\t${extension}\t${content_length}"
-    exit
+    mkdir -v "${1}"
+    cd "${1}"
+    filename="${1}${extension}"
+    echo "Downloading $filename from $url"
+    curl -s -L ${url} -o "${filename}"
+    case "${filename##*.}" in
+      zip)
+        echo "Unzipping..."
+        unzip -jq "${filename}"
+        rm "${filename}"
+        ;;
+      *)
+        echo "Skipping unzip"
+    esac
+    echo "Converting..."
+    case $extension in
+      _jp2.zip|_raw_jp2.zip)
+        for jp2 in ${1}*.jp2; do
+          convert "${jp2}" $CONVERT_OPTIONS "${jp2%.*}.png"
+          rm "${jp2}"
+        done
+        ;;
+      _tif.zip)
+        for tif in ${1}*.tif; do
+          convert "${tif}" $CONVERT_OPTIONS "${tif%.*}.png"
+          rm "${tif}"
+        done
+        ;;
+      .pdf|_bw.pdf)
+        convert -density 300 "${filename}" $CONVERT_OPTIONS "${1}_%05d.png"
+        rm "${filename}"
+        ;;
+      *)
+        echo "Unknown extension"
+    esac
+    # should have everything in Grayscale PNG at this point
+    for png in ${1}*.png; do
+      tesseract -l lat+grc+eng "${png}" "${png%.*}" hocrpdf
+      rm -v "${png}"
+    done
+    break
   fi
 done
-
-echo -e "$1\tmissing\t0"
