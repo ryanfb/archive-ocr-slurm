59d88c68863d3c52e77d37cc0ab79d1f1e46eaca
Ryan Baumann <ryan.baumann@gmail.com>
2016-02-02 11:31:58 -0500

    Switch to processing all files in dir

--- archive-ocr-slurm-runocr.sh
+++ archive-ocr-slurm-runocr.sh
@@ -4,9 +4,12 @@ SRUN_CONVERT_OPTIONS="--mem-per-cpu=768 --time=10 -n 1 -N 1"
 SRUN_TESSERACT_OPTIONS="--mem-per-cpu=256 --time=10 -n 1 -N 1"
 CONVERT_OPTIONS="-type Grayscale -background white +matte -depth 32"
 
-if [ "${filename##*.}" != ".png" ]; then
-  convert "${1}" $CONVERT_OPTIONS "${1%.*}.png"
-  rm "${1}"
-fi
-~/local/bin/tesseract -l ${2} "${1%.*}.png" "${1%.*}" hocr
-rm "${1%.*}.png"
+cd "${1}"
+find . -maxdepth 1 -name '*.png' -o -name '*.tif' -o -name '*.jp2' | while read image; do 
+  if [ "${image##*.}" != ".png" ]; then
+    convert "${image}" $CONVERT_OPTIONS "${image%.*}.png"
+    rm "${image}"
+  fi
+  ~/local/bin/tesseract -l ${2} "${image%.*}.png" "${image%.*}" hocr
+  rm "${image%.*}.png"
+done
