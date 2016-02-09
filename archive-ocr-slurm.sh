273e9af0701e80fa078b2a429245f91ee31dc9b6
Ryan Baumann <ryan.baumann@gmail.com>
2016-02-09 16:30:01 -0500

    Use placeholder files after pdf/zip explosion

--- archive-ocr-slurm.sh
+++ archive-ocr-slurm.sh
@@ -20,14 +20,19 @@ for extension in "${extensions[@]}"; do
     if [ ! -f "$filename" ]; then
       echo "Downloading $filename from $url"
       curl -s -L ${url} -o "${filename}"
-      if [ "${filename##*.}" == "zip" ]; then
-        echo "Unzipping..."
-        unzip -jq "${filename}"
-      fi
-    fi
-    if [[ "$extension" == ".pdf" || "$extension" == "_bw.pdf" ]]; then
-      echo "Converting..."
-      convert -density 300 "${filename}" $CONVERT_OPTIONS "${1}_%05d.png"
+      case "${filename##*.}" in
+        zip)
+          echo "Unzipping..."
+          unzip -jq "${filename}" && rm "${filename}" && touch "${filename}"
+          ;;
+        pdf)
+          echo "Converting..."
+          convert -density 300 "${filename}" $CONVERT_OPTIONS "${1}_%05d.png" && rm "${filename}" && touch "${filename}"
+          ;;
+        *)
+          echo "Unknown extension"
+          ;;
+      esac
     fi
     find . \( -name '*.jp2' -o -name '*.tif' -o -name '*.png' \) -print0 | xargs -0 -r -n $CHUNK_SIZE sbatch $SBATCH_OPTIONS $DIR/archive-ocr-slurm-runocr.sh "${2}"
     break
